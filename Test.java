cd0a6b4b4b04b6a1c61602d15b7762c077ec86ed
taegeon.Ahn <taegun922@gmail.com>
2020-05-06 13:01:02 +0900

    git제거

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 class Test{
 	public static void main(String [] arts){
-		System.out.println("Hello Git");
-                System.out.println("Hi Git");
+		System.out.println("Hello ");
+                System.out.println("Hi ");
 		System.out.println("branch1");
 		System.out.println("branch2");
 	}
